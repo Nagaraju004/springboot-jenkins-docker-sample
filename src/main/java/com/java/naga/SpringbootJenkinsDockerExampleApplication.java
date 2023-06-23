15565f208b36e0cd24b64437ff0a17ace47bd08d
Saravanan <nsarav756@apac.comcast.com>
2023-06-23 17:33:36 +0530

    First commit

---
+++ src/main/java/com/java/naga/SpringbootJenkinsDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.java.naga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDockerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/naga/SpringbootJenkinsDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.naga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
